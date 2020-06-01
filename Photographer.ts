ec5e71fb8f2f8078ec414184eb6672cdafa9db1a
carolinapatino <cdpatino.17@est.ucab.edu.ve>
2020-05-31 21:33:48 -0400

    Se actualizó photographer

--- Photographer.ts
+++ Photographer.ts
@@ -1,14 +1,22 @@
 import { Camera } from "./Camera";
+import { Capture } from "./Capture";
 
 export class Photographer {
   public name: string;
+  public myCamera:Camera;
+  public myCapture: Capture;
+  public CaptureType = 1;
 
   public Photographer(name: string) {
     this.name = name;
   }
 
   public turnOn(): Camera {
-    let myCamera = new Camera();
-    return myCamera;
+    this.myCamera = new Camera();
+    return this.myCamera;
+  }
+
+  public takeCapture (){
+    this.myCapture = this.myCamera.TakeCapture(this.CaptureType);
   }
 }
